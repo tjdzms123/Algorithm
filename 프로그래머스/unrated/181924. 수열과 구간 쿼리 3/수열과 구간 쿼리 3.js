49fd76f59811312ee57b6d1bee46e5c5a6fc1348
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2023-12-18 00:09:04 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.38 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,9 +1,6 @@
 function solution(arr, queries) {
-  for (let i = 0; i < queries.length; i++) {
-    [arr[queries[i][0]], arr[queries[i][1]]] = [
-      arr[queries[i][1]],
-      arr[queries[i][0]],
-    ];
-  }
-  return arr;
+  queries.forEach(([a, b]) => {
+    [arr[a], arr[b]] = [arr[b], arr[a]];
+  });
+  return arr
 }
