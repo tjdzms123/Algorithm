df7cddbb3be1d868ff001cd0afc9fa81d145e83c
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2023-12-17 22:42:16 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,11 +1,4 @@
-function solution(num_list) {
-  const lastElement = num_list.slice(-1)[0];
-  const lastSecondElement = num_list.slice(-2, -1)[0];
-  if (lastSecondElement < lastElement) {
-    num_list.push(lastElement - lastSecondElement);
-  } else {
-    num_list.push(lastElement * 2);
-  }
-
-  return num_list;
-}
+const solution = num_list => {
+  const [a, b] = [...num_list].reverse();
+  return [...num_list, a > b ? a - b : a * 2];
+};
