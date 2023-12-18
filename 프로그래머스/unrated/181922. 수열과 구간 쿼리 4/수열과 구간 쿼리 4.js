e27f7a602efb6d0295aca49d394ebecb4d18a5e9
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2023-12-18 14:48:23 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 8.01 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,11 +1,8 @@
 function solution(arr, queries) {
-  let answer = [];
-  for (let i = 0; i < queries.length; i++) {
-    const [s, e, k] = queries[i];
-    let result = Array.from({ length: e - s + 1 }, (_, idx) => s + idx).filter(
-      x => x % k == 0
-    );
-    result.map(r => (arr[r] = arr[r] + 1));
+  for (let [s, e, k] of queries) {
+    for (let i = s; i <= e; i++) {
+      if (i % k == 0) arr[i]++;
+    }
   }
-  return arr
+  return arr;
 }
