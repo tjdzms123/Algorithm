e03f76727223fa7e7672961d7314c87b4598f798
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2023-12-21 01:32:25 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,7 +1,3 @@
 function solution(x1, x2, x3, x4) {
-  let answer1 = true;
-  let answer2 = true;
-  x1 || x2 ? answer1 : (answer1 = false);
-  x3 || x4 ? answer2 : (answer2 = false);
-  return answer1 && answer2 ? true : false;
+  return (x1 || x2) && (x3 || x4) ? true : false;
 }
