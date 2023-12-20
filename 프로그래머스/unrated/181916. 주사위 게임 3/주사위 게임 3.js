0c3bc1756373bd03206dba4fee1782722ef0b90e
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2023-12-21 03:11:05 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,54 +1,21 @@
 function solution(a, b, c, d) {
-  const maxValue = Math.max(a,b,c,d) , minValue = Math.min(a,b,c,d)
-  let maxCount=0,minCount=0
-  const arr = [a,b,c,d]
-  arr.forEach((diceCount)=>{
-      if(diceCount === maxValue){
-          maxCount++
-      }
-      else if(diceCount === minValue){
-          minCount++
-      }
-  })
+  if (a == b && a == c && a == d) return 1111 * a;
 
-  // 네 주사위 같은 경우
-  if(maxCount === 4){
-      return 1111 * maxValue
-  }
+  if (a == b && a == c) return (10 * a + d) ** 2;
+  if (a == b && a == d) return (10 * a + c) ** 2;
+  if (a == c && a == d) return (10 * a + b) ** 2;
+  if (b == c && b == d) return (10 * b + a) ** 2;
 
-  // 세 주사위 같은 경우
-  if(maxCount === 3){
-      return Math.pow(10*maxValue+minValue,2)
-  }
-  if(minCount === 3){
-      return Math.pow(10*minValue+maxValue,2)
-  }
+  if (a == b && c == d) return (a + c) * Math.abs(a - c);
+  if (a == c && b == d) return (a + b) * Math.abs(a - b);
+  if (a == d && b == c) return (a + b) * Math.abs(a - b);
 
-  // 두 주사위가 두 그룹이 같은 경우
-  if(maxCount === 2 && minCount === 2){
-      return (maxValue+minValue) * Math.abs(maxValue-minValue)
-  }
+  if (a == b) return c * d;
+  if (a == c) return b * d;
+  if (a == d) return b * c;
+  if (b == c) return a * d;
+  if (b == d) return a * c;
+  if (c == d) return a * b;
 
-  // 두 주사위가 한 그룹만 같은 경우
-  if(a===b){
-      return c*d
-  }
-  if(a===c){
-      return b*d
-  }
-  if(a===d){
-      return b*c
-  }
-  if(b===c){
-      return a*d
-  }
-  if(b===d){
-      return a*c
-  }
-  if(c===d){
-      return a*b
-  }
-
-  // 그외 
-  return minValue
+  return Math.min(a, b, c, d);
 }
