b0678897c7dd14ff02ba7bc9c75234572f0067a2
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2024-01-13 18:59:00 +0900

    [level Lv.0] Title: 할 일 목록, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/Lv.0/181885. 할 일 목록/할 일 목록.js
@@ -1,9 +1,3 @@
 function solution(todo_list, finished) {
-  const answer = [];
-  for (let i = 0; i < todo_list.length; i++) {
-    if (!finished[i]) {
-      answer.push(todo_list[i]);
-    }
-  }
-  return answer;
+  return todo_list.filter((e, i) => !finished[i]);
 }
