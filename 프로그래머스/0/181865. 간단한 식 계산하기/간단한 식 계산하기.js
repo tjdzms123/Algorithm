9b974dbb5dc38dc5c2b8887998a1f9a28f5feae4
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2024-01-23 18:53:38 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,29 +1,19 @@
 function solution(binomial) {
-    // 주어진 이항식을 공백을 기준으로 나누어 배열로 저장
-    var parts = binomial.split(" ");
+  const [a, op, b] = binomial.split(' ');
 
-    // 배열에서 숫자와 연산자를 추출
-    var a = parseInt(parts[0], 10);
-    var op = parts[1];
-    var b = parseInt(parts[2], 10);
-
-    // 계산 수행
-    var result;
-    switch (op) {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        default:
-            // 유효하지 않은 연산자인 경우 에러 또는 특정 값 처리
-            console.error("유효하지 않은 연산자입니다.");
-            return null;
-    }
-
-    return result;
+  let result;
+  switch (op) {
+    case '+':
+      result = +a + +b;
+      break;
+    case '-':
+      result = +a - +b;
+      break;
+    case '*':
+      result = +a * +b;
+      break;
+    default:
+      return null;
+  }
+  return result;
 }
