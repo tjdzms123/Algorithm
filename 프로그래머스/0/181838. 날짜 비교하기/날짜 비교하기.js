9ba245bfb1f1c5e71876f4459758a6f366019d59
tjdzms123 <124573983+tjdzms123@users.noreply.github.com>
2024-01-31 20:18:20 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.15 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,6 +1,3 @@
 function solution(date1, date2) {
-  const firstDate = new Date(date1[0], date1[1] - 1, date1[2]);
-  const secondDate = new Date(date2[0], date2[1] - 1, date2[2]);
-
-  return firstDate < secondDate ? 1 : 0;
+  return new Date(date1) < new Date(date2) ? 1 : 0;
 }
